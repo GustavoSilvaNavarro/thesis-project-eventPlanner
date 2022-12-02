bc1513bfbe52ab855a3f618ddea564d106512c66
Gustavo Antonio Silva Navarro <gustavo.silva.nav@gmail.com>
2022-12-02 14:04:24 -0300

    Delete env.ts

--- frontend/src/helpers/env.ts
+++
@@ -1,6 +0,0 @@
-export const env = {
-  domain: process.env.REACT_APP_AUTH0_DOMAIN as string,
-  clientId: process.env.REACT_APP_AUTH0_CLIENT_ID as string,
-  baseUrl: process.env.REACT_APP_BASE_URL as string,
-  googleSecretKey: process.env.REACT_APP_GOOGLE_API_KEY as string,
-};
